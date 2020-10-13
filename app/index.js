154431768bd198fa6fcf34f179c950c877a86da5
Roman Oliinyk <oliinyk@oliinyk.local>
2020-10-13 11:08:05 +0300

    added slider functionality

--- app/index.js
+++ app/index.js
@@ -1,8 +1,37 @@
 const reloadIcon = document.querySelector(".generator-update-icon");
 const copyBtn = document.querySelector(".generator-copy-btn");
 
+const minusBtn = document.querySelector(".minus");
+const plusBtn = document.querySelector(".plus");
+const passLength = document.querySelector('.password-length');
+const slider = document.querySelector('.slider');
+
+minusBtn.addEventListener('click', decreasePasswordLength);
+plusBtn.addEventListener('click', incresePasswordLength);
+
+showPasswordLength(slider.value);
+
+slider.oninput = function () { 
+  passLength.textContent = this.value;
+}
+
+
 reloadIcon.addEventListener('click', rotateOn);
 copyBtn.addEventListener('click', copyPassword);
+
+function showPasswordLength(val) {
+  passLength.textContent = val;
+}
+
+function decreasePasswordLength() { 
+  const val = --slider.value;
+  showPasswordLength(val);
+}
+
+function incresePasswordLength() { 
+  const val = ++slider.value;
+  showPasswordLength(val);
+}
 
 function rotateOn(){ 
   reloadIcon.classList.add("rotate");
